fbe56f60ba38d275678ad31f5e563abbdefbd383
HaseebAhmed08 <ha9719121@gmail.com>
2024-12-07 21:27:52 +0500

    i have worked as a practice in add_value file in down side

--- add_value.dart
+++ add_value.dart
@@ -1,4 +1,4 @@
-void main (){
+// void main (){
 
 
 
@@ -9,10 +9,54 @@ void main (){
 
 
 
-List boys = ['ali','bilal','kamran','haseeb','momin','rehman'] ;
+// List boys = ['ali','bilal','kamran','haseeb','momin','rehman'] ;
 
-boys.addAll(['jameel','naeem','hashim']);
-print(boys);
+// boys.addAll(['jameel','naeem','hashim']);
+// print(boys);
+
+
+
+// }
+
+//      rough work
+
+
+
+void main (){
+
+List studentsnames = [ "ali","ahmed","ayat","kamran" ,1,23,5,4,8 ];
+
+
+print(studentsnames.contains('ali'));
+
+studentsnames.getRange(0, 4);
+ var result = studentsnames.where((e)=> e.isEven);
+ print(result);
+
+
+
+
+
+studentsnames.add('akram');
+print(studentsnames);
+
+studentsnames[2] ='jameel';
+print(studentsnames);
+
+studentsnames.addAll(['afaaq','daniyal','shayan']);
+
+print(studentsnames);
+
+print(studentsnames.length);
+
+ print("sort is here < < > >");
+studentsnames.sort();
+ print(studentsnames);
+
+ print(studentsnames.reversed);
+ 
+
+
 
 
 
