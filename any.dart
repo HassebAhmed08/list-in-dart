55699cbd998248d67ff0e31f8ee093d25894b5e8
HaseebAhmed08 <ha9179121@gmail.com>
2024-12-15 09:44:58 +0500

    i have uloded any method

---
+++ any.dart
@@ -0,0 +1,19 @@
+
+
+void main() {
+//  any mothod use for see is the vlaue >= any 
+//in list this will give us output in true / false
+
+  List num1 = [
+    12,
+    13,
+    14,
+    152,
+    124,
+    3453,
+    234,
+  ];
+  print(num1.any((e)=>e>=50));
+
+  
+}
--- reduce.dart
+++ reduce.dart
@@ -1,7 +1,22 @@
 void main() {
+  //    first method
+
   List numbers = [2, 4, 22, 75, 4, 5, 9, 3];
   var reduced = numbers.reduce((a, b) => a > b ? a : b);
   var reduced1 = numbers.reduce((a, b) => a < b ? a : b);
   print(reduced);
   print(reduced1);
+
+
+
+//   second method
+  
+  List num1 = [4, 5, 6, 7, 43, 2, 12, 35, 64, 32, 54, 3, 6, 7];
+
+  num1.sort();
+  int greatestnNumber = num1.last;
+  int smallestNumber = num1.first;
+
+  print("greatest number is $greatestnNumber");
+  print("smallest number is $smallestNumber");
 }
